fe3574c0d65cf1c893d87f97fb524776f98fb6b9
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-04-04 02:57:10 +0530

    Updated deployGrafana.groovy

--- vars/deployGrafana.groovy
+++ vars/deployGrafana.groovy
@@ -1,11 +1,29 @@
 // Defined a method to deploy Grafana using Ansible
 def call(Map configMap) {
-    // Ensuring required parameters are provided
+    // Ensure required parameters are provided
     def ansiblePlaybook = configMap.playbook ?: '/home/ubuntu/Ansible-Role-Grafana/grafana/tasks/main.yml'
     def inventory = configMap.inventory ?: '/home/ubuntu/Ansible-Role-Grafana/grafana/tests/inventory'
     def extraVars = configMap.extraVars ?: [:]
 
+    // Validate playbook and inventory file paths
+    if (!fileExists(ansiblePlaybook)) {
+        echo "Error: Playbook file not found at ${ansiblePlaybook}"
+        return
+    }
+    if (!fileExists(inventory)) {
+        echo "Error: Inventory file not found at ${inventory}"
+        return
+    }
+
     // Execute Ansible playbook to deploy Grafana
-    sh "ansible-playbook -i ${inventory} ${ansiblePlaybook} ${extraVars.collect { k, v -> "-e ${k}=${v}" }.join(' ')}"
+    try {
+        sh "ansible-playbook -i ${inventory} ${ansiblePlaybook} ${extraVars.collect { k, v -> "-e ${k}=${v}" }.join(' ')}"
+    } catch (Exception e) {
+        echo "Error executing Ansible playbook: ${e.message}"
+        return
+    }
 }
 
+def fileExists(String filePath) {
+    return new File(filePath).exists()
+}
